fc3e87f30e2256a9fd89b64a4ee47fb8b367e885
SecretCandy <parksw9762@gmail.com>
2024-06-28 13:19:59 +0900

    로직 구현 완료

--- src/api/countries.ts
+++ src/api/countries.ts
@@ -3,6 +3,5 @@ import { Country } from "../types/country";
 
 export const getCountries = async (): Promise<Country[]> => {
     const response = await axios.get("https://restcountries.com/v3.1/all");
-    console.log(response);
     return response.data;
 };
